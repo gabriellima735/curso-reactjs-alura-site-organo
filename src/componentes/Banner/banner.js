76dbd5d6434ce1a3085d4d10c58e191182f413a7
GBgabriel <gabrielllaraujo@gmail.com>
2023-03-04 23:18:12 -0300

    Aula 1-4 | Criação do componente banner

---
+++ src/componentes/Banner/banner.js
@@ -0,0 +1,14 @@
+import './Banner.css'
+
+function Banner() {
+    // JSX
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="O banner pirncipal da página do Organo"></img>
+        </header>
+        
+    )
+
+}
+
+export default Banner
